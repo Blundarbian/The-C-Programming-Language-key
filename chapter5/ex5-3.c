9faea06e60a305dbb3a9c584da68a1afe07a5f1d
Blundarbian <Jambo3657@gmail.com>
2026-03-09 16:15:05 -0700

    exercise 3 done

---
+++ chapter5/ex5-3.c
@@ -0,0 +1,33 @@
+/* Exercise 5-3. Write a pointer version of the function 
+   strcat that we showed in Chapter 2: strcat ( s, t) 
+   copies the string t to the end of s.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void stringcat(char *s, char *t) {
+	// end of s
+	while (*s) 
+		++s;
+
+	// t is copied to end of s
+	while ((*s = *t)) {
+		++s;
+		++t;
+	}
+}
+
+int main(void) {
+	char s[100] = "Five";
+	char t[] = " Six, Seven, Eight!";
+
+	printf("s is val: %s\n", s);
+	stringcat(s, t);
+
+	printf("s is now: %s\n", s);
+	
+	return 0;
+}
+
