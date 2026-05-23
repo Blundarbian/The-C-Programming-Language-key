884b64b987d58b583e8d94d5c264ee1a0a1f76f1
Blundarbian <Jambo3657@gmail.com>
2026-05-23 15:44:45 -0700

    change exercise comments and create 17th exercise

--- chapter5/ex5-15.c
+++ chapter5/ex5-15.c
@@ -1,6 +1,6 @@
-/* Exercise 5-14. Modify the sort program to handle a -r 
- * flag, which indicates sorting in reverse (decreasing) 
- * order. Be sure that -r works with -n.
+/* Exercise 5-15. Add the option -f to fold upper and lower case 
+ * together, so that case distinctions are not made during sorting; 
+ * for example, a and A compare equal.
  */
 
 #include <stdio.h>
