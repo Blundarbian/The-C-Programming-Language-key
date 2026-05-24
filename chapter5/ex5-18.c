387e9cd80123b5affd4121ed61ab102f004346a7
Blundarbian <Jambo3657@gmail.com>
2026-05-23 19:00:16 -0700

    dcl function with error checking for NAME

---
+++ chapter5/ex5-18.c
@@ -0,0 +1,23 @@
+/*Exercise 5-18. Make dcl recover from input errors.
+ */
+
+//parse the declorator 
+void dcl(void)
+{
+    int ns;
+
+    for (ns = 0; gettoken() == '*'; ) // count '*' char
+        ns++;
+    
+    if (tokentype != NAME && tokentype != '(') { // valid input
+        printf("error: NAME expected\n");
+
+        while (tokentype != '\n' && tokentype != EOF) // skip '\n'
+            tokentype = gettoken();
+
+        return;
+    }
+    dirdcl();
+    while (ns-- > 0)
+        strcat(out, " pointer to"); // apend to out
+}
