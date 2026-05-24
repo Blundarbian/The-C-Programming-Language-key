75d4486195acc6583609aadc5fa2522d1973fcb7
Blundarbian <Jambo3657@gmail.com>
2026-05-23 21:57:00 -0700

    test for erroneous parenthesis

--- chapter5/ex5-19.c
+++ chapter5/ex5-19.c
@@ -5,21 +5,36 @@
 // undcl : convert word to descriptions to declarations
 int main() {
 
-	int type;
+	int type, c1, c2;
 	char temp[MAXTOKEN];
-	
+	char *pf = "(*%s)";
+	char *df = "*s%s";
+
 	while (gettoken() != EOF) {
 		strcpy(out, token);
-		while ((type = gettoken()) != '\n') 
+		while ((type = gettoken()) != '\n') {
+			if (type == PARENS || type == BRACKETS)
+				strcat(out, token);
+			else if (type == '*') {
+				char *sf = df;
+				c1 = getchar();
+				c2 = getchar();
+
+				if (c1 == ' ' && (c2 == '(' || c2 == '[')) 
+					sf = pf;
+
+				ungetch(c2);
+				ungetch(c1);
+				sprintf(temp, sf, out);
+				strcpy(out, temp);
+
+			} else if (type == NAME) {
+				sprintf(temp, "%s %s", token, out);
+				strcpy(out, temp);
+			} else
+				printf("invalid input at %s\n", token);
+		}
+		printf("%s\n", out);
 	}
-	if (type == PARENS || type == BRACKETS)
-		strcat(out, token);
-	else if (type == '*') {
-		sprintf(temp, "(*%s)", out);
-		strcpy(out, temp);
-	} else if (type == NAME) {
-		sprintf(temp, "%s %s", token, out);
-		strcpy(out, temp);
-	} else
-		printf("invalid input at %s\n", token);
+	return 0;
 }
