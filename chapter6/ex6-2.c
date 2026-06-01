d8e4d5bd4bfb97790e56bd133da36616ba9ec1d4
Blundarbian <Jambo3657@gmail.com>
2026-06-01 11:41:49 -0700

    addtree function

--- chapter6/ex6-2.c
+++ chapter6/ex6-2.c
@@ -48,6 +48,29 @@ int main(int argc, char *argv[]) {
 }
 
 
+struct treenode *addtree(struct tnode *p, char *w) {
+
+	int cond;
+
+	if (p == NULL) {		// new word and node
+		p = talloc();
+		p->word = strdup(w);
+		p->count = 1;
+		p->left = p->right = NULL;
+	}
+	else if ((cond = strcmp(w, p->word)) == 0)
+		p->count++;		// repeated word
+
+	else if (cond < 0)	// less than words into left subtree
+		p->left = addtree(p->left, w);
+
+	else			// greater than words into right subtree
+		p->right = addtree(p->right, w);
+
+	return p;
+}
+
+
 // in-order print of tree p
 void treeprint(struct tnode *p) {
 
