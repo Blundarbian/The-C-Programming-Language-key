630b08bed7a738d92613e1501f43ce2496276b9b
Blundarbian <Jambo3657@gmail.com>
2026-06-03 13:14:15 -0700

    increment nlines/lines for line number tracking

--- chapter6/ex6-3.c
+++ chapter6/ex6-3.c
@@ -8,16 +8,46 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define SLINES 100
+int line = 0;		// current line number
+			//
+char noise[] = {"the", "and"};
+
 typedef struct tnode {
 	char *word;
 	int count;
-	int *lines;
+	int lines[SLINES]
 	int nlines;
 	struct tnode *left, *right;
 } node;
 
-struct word *nalloc(void); 
+
+node *talloc(void); 
+node *addtree(node *, char *);
 void treeprint(node *);
+
+
+node *addtree(node *p, char *s) {
+
+	int cond;	
+
+	if (p == NULL) {				// new word and node
+		p = talloc();
+		p->word = sstrdup(w);
+		p->count = 1;
+		p->lines[0] = line;
+		p->nlines = 1;
+		p->left = p-right = NULL;
+	}
+	else if ((cond = strcmp(s, p->word)) == 0) { 	// repeat	
+		p->count++;
+		p->lines[nline] = line;
+		p->nlines++;
+	}
+	else if (cond < 0) 				// less than words into left
+
+
+}
 
 void treeprint(node *p) {
 
@@ -33,8 +63,14 @@ void treeprint(node *p) {
 	}
 }
 
-struct tnode *talloc(void) {
+
+node *talloc(void) {
 	return (node *) malloc(sizeof(node)); 
+}
+
+int main() {
+
+	return 0;
 }
 
 
