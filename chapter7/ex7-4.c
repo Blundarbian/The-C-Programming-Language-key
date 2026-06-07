4a8f96c57ca5bb6f632cd4dd13a4832c1d0990d9
Blundarbian <Jambo3657@gmail.com>
2026-06-07 14:18:46 -0700

    ptr ref

--- chapter7/ex7-4.c
+++ chapter7/ex7-4.c
@@ -8,20 +8,66 @@
 
 void minscanf(char *fmt,...);
 
-void minscanf(char *fmt,...) 
-{
+void minscanf(char *fmt,...) {
+
 	va_list ap;
-	char *p, *sval;
-	int ival;
-	double dval;
+	char *p;
 
 	va_start(ap, fmt);
-	for (p = fmt; *p; p++)
-	{
+	for (p = fmt; *p; p++) {
+
 		if (*p != '%')
-		{
-			putchar(*p);		
 			continue;
+	
+		switch (*++p) {
+			case 'd' :
+				int *ip = va_arg(ap, int *);
+				scanf("%d", &ival);
+				break;
+
+			case 'f' :
+				dval = va_arg(ap, double);
+				scanf("%lf", &dval);
+				break;	
+
+			case 's' :
+				for (sval = va_arg(ap, char *); *sval; sval++)
+					getchar();
+				break;
+
+			case 'o' :
+				ival = va_arg(ap, int);
+				scanf("%o", &ival);
+				break;
+
+			case 'x' : 
+				ival = va_arg(ap, int);
+				scanf("%x", &ival);
+				break;
+
+			default :
+				getchar();
+				break;
 		}
 	}
+	va_end(ap); 
+}
+
+int main() {
+
+	char *hel;
+	int dec;
+	int oct;
+	int hex;
+
+	minscanf("%s\n", &hel);
+	printf("%s\n", hel);
+
+	minscanf("%d\n", &dec);
+
+	minscanf("%o\n", &oct);
+
+	minscanf("%x\n", &hex);
+
+	return 0;
 }
