ee2b8b09cdb70003f2f7737f6f376ff01eb5472f
Blundarbian <Jambo3657@gmail.com>
2026-06-07 21:27:37 -0700

    works with files of same length

--- chapter7/ex7-6.c
+++ chapter7/ex7-6.c
@@ -4,14 +4,18 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void opentest(FILE *, char *, char *);
+#define LINESIZE 200
 
-
-FILE *fp1, *fp2;
+FILE *opentest(char *, char *);
 
 int main(int argc, char **argv) {
 
+	FILE *fp1, *fp2;
+	char line1[LINESIZE];
+	char line2[LINESIZE];
+	int linenum = 0;
 	char *prog = argv[0];
 
 	if (argc !=  3) {
@@ -19,16 +23,30 @@ int main(int argc, char **argv) {
 		exit(1);
 
 	} else {
-		opentest(fp1, prog, argv[1]);
-		opentest(fp2, prog, argv[2]);
+		fp1 = opentest(prog, argv[1]);
+		fp2 = opentest(prog, argv[2]);
+
+		while ((fgets(line1, LINESIZE, fp1)) != NULL && fgets(line2, LINESIZE, fp2)) {
+			linenum++;
+			if (strcmp(line1, line2) != 0) {
+				printf("line number %d\n", linenum);
+				printf("file %s : %s\n", argv[1], line1);
+				printf("file %s : %s\n", argv[2], line2);
+				exit(0);
+			}
+		}
 	}
+	fclose(fp1);
+	fclose(fp2);
 	return 0;
 }
 
-void opentest(FILE *fp, char *prog, char *arg) {
+FILE *opentest(char *prog, char *arg) {
+
+	FILE *fp;
 	if ((fp = fopen(arg, "r")) == NULL) {
 		printf("error : %s cannot open %s\n", prog, arg);
 		exit(2);
 	}
-	return;
+	return fp;
 }
