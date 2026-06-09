757e2ac9454b604c607b0449767bcc3746c7fe1f
Blundarbian <Jambo3657@gmail.com>
2026-06-09 16:11:16 -0700

    prints 15 lines per page. Used fprintf to practice writing to specific streams

---
+++ chapter7/ex7-8.c
@@ -0,0 +1,64 @@
+/* Exercise 7-8. Write a program to print a set of files, 
+ * starting each new one on a new page, with a title and 
+ * a running page count for each file
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 100
+#define LINEMAX 15
+char *prog;
+
+void printpage(FILE *, FILE *);
+
+void printpage(FILE *in, FILE *out) {
+
+	int c, line = 0;
+	int pg = 1;
+
+	while ((c = getc(in)) != EOF) {
+
+		putc(c, out);
+
+		if (c == '\n') {
+			line++;
+
+			if (line == LINEMAX) {
+				fprintf(stdout, "\n\t pg %d. \n", pg);
+				pg++;
+				line = 0;
+			}
+		}
+
+	}
+}
+
+int main(int argc, char *argv[]) {
+
+	prog = argv[0];
+	int linec = 0;
+	FILE *fp;
+
+	if (argc == 1) {
+		fprintf(stderr, "error: no files\n");
+		printpage(stdin, stdout);
+	}
+	else
+		while  (--argc > 0) {
+			if ((fp = fopen(*++argv, "r")) == NULL) {
+				fprintf(stderr, "error: %s cannot open %s\n", prog, *argv);
+				exit(1);
+			}
+			else {
+				fprintf(stdout, "\n\t FILE : %s \n", argv);
+				printpage(fp, stdout);
+				fclose(fp);
+			}
+
+			if (ferror(stdout) == 0)
+				fprintf(stdout, "File %s written succesfully\n", prog);
+		}
+	return 0;
+}
