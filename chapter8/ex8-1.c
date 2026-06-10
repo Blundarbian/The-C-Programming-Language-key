41b7d6baafbf6e7d4a952ea795fbb49b70eb90c5
Blundarbian <Jambo3657@gmail.com>
2026-06-10 15:47:20 -0700

    better error handling

--- chapter8/ex8-1.c
+++ chapter8/ex8-1.c
@@ -13,13 +13,22 @@ int main(int argc, char *argv[]) {
 	int f1, n;
 	char buf[BUFSIZ];
 
-	if (argc != 2)
-		perror("Usage: file to concat");
-	if ((f1 = open(argv[1], O_RDONLY, 0)) == -1)
-		perror("cat: can't open file");
+	if (argc != 2) {
+		fprintf(stderr, "Usage: file to concat\n");
+		return 1;
+	}
+
+	if ((f1 = open(argv[1], O_RDONLY, 0)) == -1) {
+		fprintf(stderr, "cat: can't open %s file\n", argv[1]);
+		return 2;
+	}
 
 	while ((n = read(f1, buf, BUFSIZ)) > 0)
-		if (write(1, buf, n) != n)
-			perror("cat: write to stdout error");
+		if (write(1, buf, n) != n) {
+			fprintf(stderr, "cat: error writing to stdout\n");
+			return 3;
+		}
+	close(f1);
+
 	return 0;
 }
