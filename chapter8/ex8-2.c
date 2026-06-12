f37a6ab3df89e396b1c784918a555183d4b2fcd8
Blundarbian <Jambo3657@gmail.com>
2026-06-11 23:58:18 -0700

    exercise 2

---
+++ chapter8/ex8-2.c
@@ -0,0 +1,15 @@
+/* Exercise 8-2. Rewrite fopen and _fillbuf with fields instead 
+ * of explicit bit operations. Compare code size and execution speed.
+ */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+
+	return 0;
+}
+
+
+
